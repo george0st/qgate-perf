66a90012fede31d5ffa63ea22c7013d5909b83f0
jist <95856749+george0st@users.noreply.github.com>
2023-10-15 17:12:02 +0200

    Tune test of Graph generation

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,3 +1,4 @@
+import glob
 import unittest
 from qgate_perf.parallel_executor import ParallelExecutor
 from qgate_perf.parallel_probe import ParallelProbe
@@ -32,12 +33,12 @@ def prf_test(run_setup: RunSetup) -> ParallelProbe:
         if probe.stop():
             break
 
-    # generate random exception
+    # generate exception
     gen_err=run_setup.param("generate_error")
     if gen_err =="yes":
         raise Exception('Simulated exception')
     elif gen_err == "random":
-        if generator.integers(0,4)==0:
+        if generator.integers(0,5)==0:
             raise Exception('Random exception')
 
     return probe
@@ -55,7 +56,7 @@ class TestCaseGraph(unittest.TestCase):
 
     def test_graph(self):
         generator = ParallelExecutor(prf_test,
-                                     label="test",
+                                     label="test_aus_ger",
                                      detail_output=True,
                                      output_file=path.join(self.OUTPUT_ADR, "perf_test.txt"))
 
@@ -66,9 +67,17 @@ class TestCaseGraph(unittest.TestCase):
                                                     setup))
         generator.create_graph(self.OUTPUT_ADR)
 
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_aus_ger-*-bulk-10x10.png"))
+        self.assertTrue(len(file) == 1)
+        print(file[0])
+
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_aus_ger-*-bulk-100x10.png"))
+        self.assertTrue(len(file) == 1)
+        print(file[0])
+
     def test_graph_run_exception(self):
         generator = ParallelExecutor(prf_test,
-                                     label="test",
+                                     label="test_exception",
                                      detail_output=True,
                                      output_file=path.join(self.OUTPUT_ADR, "perf_test.txt"))
 
@@ -78,9 +87,13 @@ class TestCaseGraph(unittest.TestCase):
         self.assertFalse(generator.run(1,1, setup))
         generator.create_graph(self.OUTPUT_ADR)
 
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_exception-*-bulk-1x1.png"))
+        self.assertTrue(len(file) == 1)
+        print(file[0])
+
     def test_graph_runexecutor_exception(self):
         generator = ParallelExecutor(prf_test,
-                                     label="test",
+                                     label="test_exception2",
                                      detail_output=True,
                                      output_file=path.join(self.OUTPUT_ADR, "perf_test.txt"))
 
@@ -89,6 +102,11 @@ class TestCaseGraph(unittest.TestCase):
         setup=RunSetup(duration_second=4, start_delay=2, parameters=setting)
         self.assertFalse(generator.run_executor([[2,2]], setup))
         generator.create_graph(self.OUTPUT_ADR)
+
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_exception2-*-bulk-1x1.png"))
+        self.assertTrue(len(file) == 1)
+        print(file[0])
+
 
     def test_graph_runbulkexecutor_exception_random(self):
         generator = ParallelExecutor(prf_test,
@@ -101,3 +119,12 @@ class TestCaseGraph(unittest.TestCase):
         setup=RunSetup(duration_second=4, start_delay=2, parameters=setting)
         generator.run_bulk_executor([[1,1], [1,5]], [[4,4],[8,4],[16,4]], setup)
         generator.create_graph(self.OUTPUT_ADR)
+
+        # check relevant files
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_random-*-bulk-1x1.png"))
+        self.assertTrue(len(file)==1)
+        print(file[0])
+
+        file=glob.glob(path.join(self.OUTPUT_ADR, "graph-perf",f"PRF-test_random-*-bulk-1x5.png"))
+        self.assertTrue(len(file) == 1)
+        print(file[0])
