5cad2083435ee1e1188a2caa31ca26ac54aacfbf
jist <95856749+george0st@users.noreply.github.com>
2023-10-20 15:02:53 +0200

    Switch to new deployment (toml)

--- publish.bat
+++ publish.bat
@@ -5,19 +5,18 @@ rmdir /S /Q dist
 rmdir /S /Q build
 
 rem 'setup.py bdist_wheel' is deprecated recommendation is to use 'python -m build'
-python setup.py bdist_wheel
+rem python -m build --wheel
+rem py -m build
 
-rem FULL DESCRIPTION
-rem https://packaging.python.org/en/latest/tutorials/packaging-projects/
-rem new way 'pyproject.toml' + 'python -m build.'
+rem helper 'https://www.scivision.dev/python-minimal-package/'
+rem https://pypa-build.readthedocs.io/en/latest/
+rem https://github.com/pypa/build
+python -m build --sdist
 
-rem or https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-rem SAMPLE https://github.com/pypa/sampleproject/blob/main/pyproject.toml
+rem  --no-isolation
+rem --sdist, -s - build a source distribution (disables the default behavior)
+rem --wheel, -w - build a wheel (disables the default behavior)
 
-
-rem So keep the version only in the pyproject.toml and use this in your python code:
-rem import importlib.metadata
-rem __version__ = importlib.metadata.version(__package__ or __name__)
 
 rem twine upload is supported
-twine upload dist/* --verbose -ugeorge0st
+twine upload dist/* --verbose -u__token__
