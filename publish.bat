a484a08b37994ed7e36676dedf028464c45b97ce
jist <95856749+george0st@users.noreply.github.com>
2023-10-20 15:56:54 +0200

    Tune build new package

--- publish.bat
+++ publish.bat
@@ -3,6 +3,7 @@ rem pip install --upgrade twine
 
 rmdir /S /Q dist
 rmdir /S /Q build
+rmdir /S /Q qgate_perf.egg-info
 
 rem 'setup.py bdist_wheel' is deprecated recommendation is to use 'python -m build'
 rem python -m build --wheel
@@ -11,7 +12,7 @@ rem py -m build
 rem helper 'https://www.scivision.dev/python-minimal-package/'
 rem https://pypa-build.readthedocs.io/en/latest/
 rem https://github.com/pypa/build
-python -m build --sdist --wheel
+python -m build --sdist
 
 rem  --no-isolation
 rem --sdist, -s - build a source distribution (disables the default behavior)
