b8419af2f075528773fffe63f9a145cce00b18a2
jist <95856749+george0st@users.noreply.github.com>
2024-09-28 17:23:08 +0200

    Update test_simulation.py

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -35,8 +35,8 @@ class TestCasePerf(unittest.TestCase):
         expected ={}
         expected['call'] = len(sequence)
         expected['avr'] = float(round(np.average(sequence), ParallelProbe.HUMAN_PRECISION))
-        expected['min'] = float(np.min(sequence))
-        expected['max'] = float(np.max(sequence))
+        expected['min'] = float(round(np.min(sequence), ParallelProbe.HUMAN_PRECISION))
+        expected['max'] = float(round(np.max(sequence), ParallelProbe.HUMAN_PRECISION))
         expected['std'] = float(round(np.std(sequence), ParallelProbe.HUMAN_PRECISION))
         expected['total'] = float(round(np.sum(sequence),ParallelProbe.HUMAN_PRECISION))
 
@@ -45,15 +45,57 @@ class TestCasePerf(unittest.TestCase):
 
         self.assertTrue(simulate.counter == expected['call'])
         self.assertTrue(round(simulate.total_duration / simulate.counter, ParallelProbe.HUMAN_PRECISION) == expected['avr'])
-        self.assertTrue(simulate.min_duration == expected['min'])
-        self.assertTrue(simulate.max_duration == expected['max'])
+        self.assertTrue(round(simulate.min_duration, ParallelProbe.HUMAN_PRECISION) == expected['min'])
+        self.assertTrue(round(simulate.max_duration, ParallelProbe.HUMAN_PRECISION) == expected['max'])
         self.assertTrue(round(simulate.standard_deviation,ParallelProbe.HUMAN_PRECISION) == expected['std'])
         self.assertTrue(round(simulate.total_duration, ParallelProbe.HUMAN_PRECISION) == expected['total'])
 
 
-    def test_basic_statistic(self):
+    def test_basic_statistic1(self):
         sequence = [0.24, 0.21, 0.34, 0.33]
 
         simulate=SimulateProbe()
         simulate.run(sequence)
         self._check(simulate, sequence)
+
+    def test_basic_statistic2(self):
+        sequence = [0.24, 0.21, 0.34, 0.33, 0.345, 0.11, 0.232435, 0.2344, 1.4, 2.455]
+
+        simulate=SimulateProbe()
+        simulate.run(sequence)
+        self._check(simulate, sequence)
+
+    def test_basic_statistic3(self):
+        sequence = [0.24, 0.21, 0.34, 0.33, 0.345, 0.11, 0.232435, 0.2344, 1.4, 2.455, 88, 99, 1995, 334, 222, 4.33]
+
+        simulate=SimulateProbe()
+        simulate.run(sequence)
+        self._check(simulate, sequence)
+
+    def test_basic_statistic4(self):
+        sequence = [0.24, 0.21, 0.34, 0.33, 0.345, 0.11, 0.232435, 0.2344, 1.4, 2.455, 88, 99, 1995, 334, 222, 4.33,
+                    0.222, 0.2323, 0.456, 0.545, 0.332]
+
+        simulate=SimulateProbe()
+        simulate.run(sequence)
+        self._check(simulate, sequence)
+
+    def test_basic_statistic5(self):
+        sequence = [0.24, 0.21, 0.34, 0.33, 0.345, 0.11, 0.232435, 0.2344, 1.4, 2.455, 88, 99, 1995, 334, 222, 4.33,
+                    0.222, 0.2323, 0.456, 0.545, 0.332, 7.334, 3.454, 7.3333, 0.3344, 0.576565, 0.8787]
+
+        simulate=SimulateProbe()
+        simulate.run(sequence)
+        self._check(simulate, sequence)
+
+    def test_basic_statistic6(self):
+        sequence = [0.24, 0.21, 0.34, 0.33, 0.345, 0.11, 0.232435, 0.2344, 1.4, 2.455, 88, 99, 1995, 334, 222, 4.33,
+                    0.222, 0.2323, 0.456, 0.545, 0.332, 7.334, 3.454, 7.3333, 0.3344, 0.576565, 0.8787, 1.1, 1.1, 1.4,
+                    1.3, 1.5, 3.2, 0.44]
+
+        simulate=SimulateProbe()
+        simulate.run(sequence)
+        self._check(simulate, sequence)
+
+    def test_random_statistic(self):
+        pass
