17e042c08e2faef79f0ffd3c4d0361a7012f14a8
jist <95856749+george0st@users.noreply.github.com>
2024-09-28 15:02:57 +0200

    Create test_simulation.py

---
+++ tests/test_simulation.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestCasePerf(unittest.TestCase):
+
+    OUTPUT_ADR = "../output/test_perf/"
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_basic_statistic(self):
+        pass
