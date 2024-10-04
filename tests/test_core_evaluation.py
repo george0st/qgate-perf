ed91bd1d01c17ef8a071a0923fbd6bcabc30d49b
jist <95856749+george0st@users.noreply.github.com>
2024-10-04 15:01:16 +0200

    Add unit tests

--- tests/test_core_evaluation.py
+++ tests/test_core_evaluation.py
@@ -2,10 +2,11 @@ import unittest
 from qgate_perf.parallel_executor import ParallelExecutor
 from qgate_perf.parallel_probe import ParallelProbe
 from qgate_perf.run_setup import RunSetup
+from qgate_perf.output_setup import OutputSetup
 from time import sleep
 import shutil
 
-def prf_calibration_onehundred_ms(run_setup: RunSetup) -> ParallelProbe:
+def prf_calibration_100_ms(run_setup: RunSetup) -> ParallelProbe:
     """ Function for performance testing"""
 
     # init (contain executor synchronization, if needed)
@@ -31,7 +32,7 @@ def prf_calibration_onehundred_ms(run_setup: RunSetup) -> ParallelProbe:
     # return outputs
     return probe
 
-def prf_calibration_ten_ms(run_setup: RunSetup) -> ParallelProbe:
+def prf_calibration_10_ms(run_setup: RunSetup) -> ParallelProbe:
     """ Function for performance testing"""
 
     # init (contain executor synchronization, if needed)
@@ -57,6 +58,31 @@ def prf_calibration_ten_ms(run_setup: RunSetup) -> ParallelProbe:
     # return outputs
     return probe
 
+def prf_calibration_4_ms(run_setup: RunSetup) -> ParallelProbe:
+    """ Function for performance testing"""
+
+    # init (contain executor synchronization, if needed)
+    probe = ParallelProbe(run_setup)
+
+    if run_setup.is_init:
+        print(f"!!! INIT CALL !!!   {run_setup.bulk_row} x {run_setup.bulk_col}")
+
+    while 1:
+
+        # START - performance measure for specific part of code
+        probe.start()
+
+        sleep(0.004)
+
+        # STOP - performance measure specific part of code
+        if probe.stop():
+            break
+
+    if run_setup.param("generate_error"):
+        raise Exception('Simulated error')
+
+    # return outputs
+    return probe
 
 class TestCaseCoreEvaluation(unittest.TestCase):
     """
@@ -71,6 +97,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        OutputSetup().human_precision=7
         shutil.rmtree(TestCaseCoreEvaluation.OUTPUT_ADR, True)
 
     @classmethod
@@ -79,7 +106,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
 
     def test_expected_output100ms_1(self):
 
-        generator = ParallelExecutor(prf_calibration_onehundred_ms,
+        generator = ParallelExecutor(prf_calibration_100_ms,
                                      label = "GIL_impact",
                                      detail_output = True,
                                      output_file = None)
@@ -112,7 +139,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
         self.assertTrue(perf[0].calls_sec >= 9 and perf[0].calls_sec <= 10)
 
     def test_expected_output100ms_2(self):
-        generator = ParallelExecutor(prf_calibration_onehundred_ms,
+        generator = ParallelExecutor(prf_calibration_100_ms,
                                      label="GIL_impact",
                                      detail_output=True,
                                      output_file = None)
@@ -144,7 +171,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
         self.assertTrue(perf[0].calls_sec >= 19 and perf[0].calls_sec <= 20)
 
     def test_expected_output100ms_3(self):
-        generator = ParallelExecutor(prf_calibration_onehundred_ms,
+        generator = ParallelExecutor(prf_calibration_100_ms,
                                      label="GIL_impact",
                                      detail_output=True,
                                      output_file = None)
@@ -178,7 +205,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
 
     def test_expected_output10ms_1(self):
 
-        generator = ParallelExecutor(prf_calibration_ten_ms,
+        generator = ParallelExecutor(prf_calibration_10_ms,
                                      label = "GIL_impact",
                                      detail_output = True,
                                      output_file = None)
@@ -212,7 +239,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
 
     def test_expected_output10ms_2(self):
 
-        generator = ParallelExecutor(prf_calibration_ten_ms,
+        generator = ParallelExecutor(prf_calibration_10_ms,
                                      label = "GIL_impact",
                                      detail_output = True,
                                      output_file = None)
@@ -246,7 +273,7 @@ class TestCaseCoreEvaluation(unittest.TestCase):
 
     def test_expected_output10ms_3(self):
 
-        generator = ParallelExecutor(prf_calibration_ten_ms,
+        generator = ParallelExecutor(prf_calibration_10_ms,
                                      label = "GIL_impact",
                                      detail_output = True,
                                      output_file = None)
@@ -277,3 +304,105 @@ class TestCaseCoreEvaluation(unittest.TestCase):
                                                   return_performance = True)
         self.assertTrue(state)
         self.assertTrue(perf[0].calls_sec >= 360 and perf[0].calls_sec <= 400)
+
+    def test_expected_output1ms_1(self):
+
+        generator = ParallelExecutor(prf_calibration_4_ms,
+                                     label = "GIL_impact",
+                                     detail_output = True,
+                                     output_file = None)
+
+        # first
+        setup=RunSetup(duration_second = 1, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                         executor_list = [[1,1]],
+                                                         run_setup = setup,
+                                                         return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 200 and perf[0].calls_sec <= 250)
+
+        # second
+        setup=RunSetup(duration_second = 2, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[1,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 200 and perf[0].calls_sec <= 250)
+
+        # third
+        setup=RunSetup(duration_second = 10, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[1,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 200 and perf[0].calls_sec <= 250)
+
+    def test_expected_output1ms_2(self):
+
+        generator = ParallelExecutor(prf_calibration_4_ms,
+                                     label = "GIL_impact",
+                                     detail_output = True,
+                                     output_file = None)
+
+        # first
+        setup=RunSetup(duration_second = 1, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                         executor_list = [[2,1]],
+                                                         run_setup = setup,
+                                                         return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 400 and perf[0].calls_sec <= 500)
+
+        # second
+        setup=RunSetup(duration_second = 2, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[2,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 400 and perf[0].calls_sec <= 500)
+
+        # third
+        setup=RunSetup(duration_second = 10, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[2,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 400 and perf[0].calls_sec <= 500)
+
+    def test_expected_output1ms_3(self):
+
+        generator = ParallelExecutor(prf_calibration_4_ms,
+                                     label = "GIL_impact",
+                                     detail_output = True,
+                                     output_file = None)
+
+        # first
+        setup=RunSetup(duration_second = 1, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                         executor_list = [[4,1]],
+                                                         run_setup = setup,
+                                                         return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 800 and perf[0].calls_sec <= 1000)
+
+        # second
+        setup=RunSetup(duration_second = 2, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[4,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 800 and perf[0].calls_sec <= 1000)
+
+        # third
+        setup=RunSetup(duration_second = 10, start_delay = 0)
+        state, perf = generator.run_bulk_executor(bulk_list = [[1,1]],
+                                                  executor_list = [[4,1]],
+                                                  run_setup = setup,
+                                                  return_performance = True)
+        self.assertTrue(state)
+        self.assertTrue(perf[0].calls_sec >= 800 and perf[0].calls_sec <= 1000)
